9cb40c89d44cdc121abc012b6f6bd55e92c97064
Brycen Anderson <114258995+Bryruto@users.noreply.github.com>
2026-04-11 14:27:36 -0600

    Add: two-integer-sum-ii - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-7.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        for(size_t i{};i<numbers.size();i++)
+            for(size_t j = i + 1;j < numbers.size();j++)
+                if(numbers[i] + numbers[j] == target) return {i+1,j+1};
+}};
