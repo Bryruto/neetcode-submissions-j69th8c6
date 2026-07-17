7c4078ef4786175e961ac8ddaf72413117506262
Brycen Anderson <114258995+Bryruto@users.noreply.github.com>
2026-07-17 14:14:30 -0600

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& s) {
+        priority_queue<int> stones(s.begin(),s.end());
+        
+        
+        while(stones.size() > 1){
+        
+            int a = stones.top();
+            stones.pop();
+            int b = stones.top();
+            stones.pop();
+
+            if(a-b > 0)
+                stones.push(a - b);
+        }
+
+        return stones.empty() ? 0 : stones.top();
+    }
+};
